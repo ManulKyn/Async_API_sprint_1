fee4b2f3d399b2d370c8a67681596fae32f76794
Andrey S <andrey.salinov@yandex.ru>
2022-05-10 18:40:04 +0300

    Добавление модели фильмов и апи для обработки запросов

--- service/api/v1/films.py
+++ service/api/v1/films.py
@@ -1,25 +1,56 @@
-from fastapi import APIRouter
-from pydantic import BaseModel
+from http import HTTPStatus
 
-# Объект router, в котором регистрируем обработчики
+from fastapi import APIRouter, Depends, HTTPException, Query
+from pydantic import BaseModel
+from uuid import UUID
+from typing import List, Optional
+
+from services.films import FilmService, get_film_service
+
 router = APIRouter()
 
-# FastAPI в качестве моделей использует библиотеку pydantic
-# https://pydantic-docs.helpmanual.io
-# У неё есть встроенные механизмы валидации, сериализации и десериализации
-# Также она основана на дата-классах
 
-# Модель ответа API
-class Film(BaseModel):
-    id: str
+class FilmMain(BaseModel):
+    uuid: UUID
     title: str
+    imdb_rating: float
 
-# С помощью декоратора регистрируем обработчик film_details
-# На обработку запросов по адресу <some_prefix>/some_id
-# Позже подключим роутер к корневому роутеру
-# И адрес запроса будет выглядеть так — /api/v1/film/some_id
-# В сигнатуре функции указываем тип данных, получаемый из адреса запроса (film_id: str)
-# И указываем тип возвращаемого объекта — Film
-@router.get('/{film_id}', response_model=Film)
-async def film_details(film_id: str) -> Film:
-    return Film(id='some_id', title='some_title')
+
+class FilmDetail(FilmMain):
+    description: Optional[str] = ''
+    genre: List[dict]
+    actors: List[dict]
+    writers: List[dict]
+    directors: List[dict]
+
+
+@router.get('/search/')
+async def film_search(
+        query: str,
+        page_size: int = Query(50, alias="page[size]"),
+        page_number: int = Query(1, alias="page[number]"),
+        film_service: FilmService = Depends(get_film_service)) -> List[FilmMain]:
+    films_all_fields_search = await film_service.get_film_search(query, page_size, page_number)
+    films = [FilmMain(uuid=x.id, title=x.title, imdb_rating=x.imdb_rating) for x in films_all_fields_search]
+    return films
+
+
+@router.get('/<uuid:UUID>/', response_model=FilmDetail)
+async def film_details(film_id: str, film_service: FilmService = Depends(get_film_service)) -> FilmDetail:
+    film = await film_service.get_by_id(film_id)
+    if not film:
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail='film not found')
+
+    return FilmDetail(uuid=film.id, title=film.title, imdb_rating=film.imdb_rating, description=film.description,
+                      genre=film.genres, actors=film.actors, writers=film.writers, directors=film.directors)
+
+
+@router.get('/')
+async def film_main(sort: str = "-imdb_rating",
+                    page_size: int = Query(50, alias="page[size]"),
+                    page_number: int = Query(1, alias="page[number]"),
+                    filter_genre: str = Query(None, alias="filter[genre]"),
+                    film_service: FilmService = Depends(get_film_service)) -> List[FilmMain]:
+    films_all_fields = await film_service.get_film_pagination(sort, page_size, page_number, filter_genre)
+    films = [FilmMain(uuid=x.id, title=x.title, imdb_rating=x.imdb_rating) for x in films_all_fields]
+    return films
--- service/main.py
+++ service/main.py
@@ -2,7 +2,7 @@ import logging
 
 import aioredis
 import uvicorn
-from elasticsearch_async import AsyncElasticsearch
+from elasticsearch import AsyncElasticsearch
 from typing import Optional
 from fastapi import Depends, FastAPI
 from fastapi.responses import ORJSONResponse
@@ -20,49 +20,50 @@ app = FastAPI(
     default_response_class=ORJSONResponse,
 )
 
+app.include_router(
+    films.router,
+    prefix='/api/v1/films',
+    tags=['films'],
+)
+#
+# app.include_router(
+#     persons.router,
+#     prefix='/api/v1/persons',
+#     tags=['persons'],
+# )
+#
+# app.include_router(
+#     genres.router,
+#     prefix='/api/v1/genres',
+#     tags=['genres'],
+# )
+
 
 @app.on_event('startup')
 async def startup():
-    # Подключаемся к базам при старте сервера
-    # Подключиться можем при работающем event-loop
-    # Поэтому логика подключения происходит в асинхронной функции
-    redis.redis = await aioredis.Redis.from_url(f"redis://{config.REDIS_HOST}:{config.REDIS_PORT}", minsize=10, maxsize=20)
-    elastic.es = AsyncElasticsearch(hosts=[f'{config.ELASTIC_HOST}:{config.ELASTIC_PORT}'])
+    redis.redis = await aioredis.from_url(
+        f'redis://{config.REDIS_HOST}:{config.REDIS_PORT}',
+        encoding="utf-8",
+        decode_responses=True,
+    )
+    elastic.es = AsyncElasticsearch(
+        hosts=[f'{config.ELASTIC_HOST}:{config.ELASTIC_PORT}'],
+    )
 
 
 @app.on_event('shutdown')
 async def shutdown():
-    # Отключаемся от баз при выключении сервера
     await redis.redis.close()
     await elastic.es.close()
 
 
-async def common_parameters(q: Optional[str] = None, skip: int = 0, limit: int = 100):
-    return {"q": q, "skip": skip, "limit": limit}
-
-
-@app.get("/items/")
-async def read_items(commons: dict = Depends(common_parameters)):
-    return commons
-
-
-@app.get("/users/")
-async def read_users(commons: dict = Depends(common_parameters)):
-    return commons
-
-# Подключаем роутер к серверу, указав префикс /v1/films
-# Теги указываем для удобства навигации по документации
-app.include_router(films.router, prefix='/api/v1/films', tags=['films'])
-
 if __name__ == '__main__':
-    # Приложение может запускаться командой
-    # `uvicorn main:app --host 0.0.0.0 --port 8000 --reload`
-    # но чтобы не терять возможность использовать дебагер,
-    # запустим uvicorn сервер через python
     uvicorn.run(
         'main:app',
         host='0.0.0.0',
         port=8000,
-        log_config=LOGGING,
-        log_level=logging.DEBUG,
+        # log_config=LOGGING,
+        # log_level=logging.DEBUG,
+        # reload=True,
+        # debug=True,
     )
